73ec0160064b608e7b53a9861af82a68e3611049
etovityusha <numbernine27@gmail.com>
2022-05-28 20:48:11 +0400

    alembic initial migration

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+from .player import Player
+from .tournament import Tournament
+from .tournament_player import TournamentPlayer
+from .tournament_player_deck import TournamentPlayerDeck
+
+__all__ = [Player, Tournament, TournamentPlayer, TournamentPlayerDeck]
