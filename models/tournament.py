6084e54cb292688985367410d366c8782ed4a3f3
etovityusha <numbernine27@gmail.com>
2022-05-28 20:39:48 +0400

    add tournament status

--- models/tournament.py
+++ models/tournament.py
@@ -11,9 +11,16 @@ class Tournament(BaseModelORM):
         EUROPE = 'EUROPE'
         AMERICAS = 'AMERICAS'
 
+    class TournamentStatusEnum(str, enum.Enum):
+        SCHEDULED = 'SCHEDULED'
+        IN_PROGRESS = 'IN_PROGRESS'
+        COMPLETED = 'COMPLETED'
+        CANCELLED = 'CANCELLED'
+
     __tablename__ = 'tournaments'
 
     battlefy_id = sa.Column(sa.String, nullable=False)
     start_time = sa.Column(sa.DateTime, nullable=False)
     name = sa.Column(sa.String, nullable=False)
     region = sa.Column(sa.Enum(RegionEnum), default=RegionEnum.EUROPE, nullable=False)
+    status = sa.Column(sa.Enum(TournamentStatusEnum), default=TournamentStatusEnum.SCHEDULED, nullable=False)
